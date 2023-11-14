d3d88c7fdd42a6c94b702e27207d52331cdb69b0
gustttaavo <142694619+gustttaavo@users.noreply.github.com>
2023-11-13 23:37:26 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
     const registroForm = document.querySelector('form');
     const registrarButton = document.querySelector('button[type="submit"]');
+    const txtSenha = document.getElementById('txtSenha');
 
     registrarButton.addEventListener('click', function(e) {
         e.preventDefault();
@@ -23,14 +24,11 @@ document.addEventListener('DOMContentLoaded', function() {
             senha: senha
         };
 
-        // Carregar o arquivo JSON existente
         fetch('teste.json')
             .then(response => response.json())
             .then(data => {
-                // Adicionar o novo usuário à matriz "usuarios"
                 data.usuarios.push(novoUsuario);
 
-                // Atualizar o arquivo JSON com os novos dados
                 return fetch('teste.json', {
                     method: 'PUT',
                     body: JSON.stringify(data),
@@ -46,4 +44,23 @@ document.addEventListener('DOMContentLoaded', function() {
                 console.error('Erro:', error);
             });
     });
+
+    // Caps Lock Check
+    txtSenha.onkeyup = function(e) {
+        var key = e.charCode || e.keyCode;
+        
+        // Enter, caps lock, and backspace are not of interest
+        if (key == 13 || key == 8 || key == 46 || key == 20) {
+            return false;
+        }
+
+        // Get the last character typed
+        var tamanho = this.value.length;
+        var ultimo_caracter = this.value.substring(tamanho - 1);
+
+        // Check if it's uppercase and not with shift key
+        if (ultimo_caracter.toUpperCase() == ultimo_caracter && ultimo_caracter.toLowerCase() != ultimo_caracter && !e.shiftKey) {
+            alert('Caps Lock está pressionado!');
+        }
+    };
 });
